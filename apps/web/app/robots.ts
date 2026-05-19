398b54d7a518784fba52299ab3ebe85c783d8b62
meowtwit <meowtwit222@gmail.com>
2026-05-20 01:01:42 +0900

    feat: sitemap.xmlとrobots.txtを追加 (#103)

---
+++ apps/web/app/robots.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from "next";
+
+const SITE_URL = "https://aiment.jp";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: [
+          "/api/",
+          "/admin/",
+          "/dev/",
+          "/wireframes/",
+          "/account/",
+          "/setup/",
+          "/studio/",
+          "/auth/",
+        ],
+      },
+    ],
+    sitemap: `${SITE_URL}/sitemap.xml`,
+  };
+}
